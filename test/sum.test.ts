46bb9dd0c339f8d8c28fa93ee892f69a40a9a5c6
aldairandre <aldairandre99@gmail.com>
2022-06-25 02:09:17 +0100

    add comentario

--- test/sum.test.ts
+++ test/sum.test.ts
@@ -2,6 +2,4 @@ import { sum } from "../src";
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1,2)).toBe(3)
-})
-
-
+}) // toBe utiliza Object.is para testar a igualdade exata. Se você quer checar o valor de um objeto, use toEqual
